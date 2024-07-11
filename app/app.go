bf40e85c8252284412e39879ec4983f747576abb
jjhonny <ojhonny1@hotmail.com>
2024-07-11 09:23:07 -0400

    feat: create the ip search functionality

--- app/app.go
+++ app/app.go
@@ -1,6 +1,10 @@
 package app
 
 import (
+	"fmt"
+	"log"
+	"net"
+
 	"github.com/urfave/cli"
 )
 
@@ -10,5 +14,32 @@ func Gerar() *cli.App {
 	app.Name = "Aplicação de Linha de Comando"
 	app.Usage = "Busca Ips e Nomes de Servidor na internet"
 
+	app.Commands = []cli.Command{
+		{
+			Name: "ip",
+			Usage: "Busca IPS de endereços na internet",
+			Flags: []cli.Flag{
+				cli.StringFlag{
+					Name: "host",
+					Value: "github.com",
+				},
+			},
+			Action: buscarIps,
+		},
+	}
+
 	return app
+}
+
+func buscarIps(c *cli.Context) {
+	host := c.String("host")
+
+	ips, erro := net.LookupIP(host)
+	if erro != nil {
+		log.Fatal(erro)
+	}
+
+	for _, ip := range ips {
+		fmt.Println(ip)
+	}
 }
